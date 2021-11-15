e48673d823d00f97a93404644350baaeb233beda
Mark Dziuban <markdziuban@gmail.com>
2021-11-15 17:27:12 -0500

    initial commit

---
+++ TripleX.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+    int difficulty = 2;
+    int maxDifficulty = 10;
+    while (difficulty <= maxDifficulty)
+    {
+
+    }
+
+    return 0;
+}
